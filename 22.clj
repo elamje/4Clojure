b07bea07c0bfafa6dd13a9188acba6ff7f2cf4f0
j3elam <j3elam@gmail.com>
2019-04-28 16:23:11 -0500

    22 v2

--- 22.clj
+++ 22.clj
@@ -6,3 +6,10 @@
 ;; count
 ;; function - doesn't handle count = 0
 #(let [x % len 1] (loop [x x len len] (if (empty? (rest x)) len (recur (rest x) (inc len)))))
+
+;; v2
+(= ((fn [x] (reduce + (map #(if (nil? %) 0 1) x))) '(1 2 3 3 1)) 5)
+;; => true
+
+;; count v2
+(fn [x] (reduce + (map #(if (nil? %) 0 1) x)))
