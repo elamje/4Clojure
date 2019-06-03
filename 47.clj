87cc3f0073f548fead3eed76fe6c63ad763293e8
j3elam <j3elam@gmail.com>
2019-06-03 11:56:44 -0500

    47

--- 47.clj
+++ 47.clj
@@ -1,1 +1,7 @@
 (ns fortyseven)
+
+(contains? #{4 5 6} 4)
+
+(contains? [1 1 1 1 1] 4) ;; contains checks if INDEX exists for vectors and lists, not if the value is contained
+
+(not (contains? [1 2 4] 4)) ;; index dne here
