d4bfd550249ffd43b33ff7fe99820511b8d1c76c
j3elam <j3elam@gmail.com>
2019-07-05 09:45:03 -0500

    44

--- 44.clj
+++ 44.clj
@@ -1,2 +1,11 @@
 (ns fortyfour)
 
+(defn rot [n coll]
+        (let [n (rem n (count coll))] (if (pos? n)
+                                        (concat (drop n coll) (take n coll))
+                                        (concat (take-last (* -1 n) coll) (take (+ (count coll) n) coll)))))
+
+(= (rot 1 '(:a :b :c)) '(:b :c :a))
+
+
+
